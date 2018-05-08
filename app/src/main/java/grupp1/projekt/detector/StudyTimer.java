3b117eab343033c7063feae14cdf0b7d652076dc
Niklas Lindvall <nic.kingsblood@gmail.com>
2018-05-08 16:35:40 +0200

    Memory storage for multiple days

--- app/src/main/java/grupp1/projekt/detector/StudyTimer.java
+++ app/src/main/java/grupp1/projekt/detector/StudyTimer.java
@@ -1,14 +1,22 @@
 package grupp1.projekt.detector;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
 public class StudyTimer {
-    private int totalStudied;
+    private DateFormat mDateFormat;
+
     private Long lastStart;
+    private HashMap<String, Integer> mStorage;
 
     public StudyTimer() {
-        totalStudied = 0;
         lastStart = null;
+        mStorage = new HashMap<>();
+        mDateFormat = new SimpleDateFormat("YYYY-MM-DD", Locale.getDefault());
     }
 
     public void start() {
@@ -18,12 +26,20 @@ public class StudyTimer {
     public void stop() {
         long stop = System.currentTimeMillis();
         if (lastStart != null && lastStart < stop) {
+            int totalStudied = getTotalStudied();
             totalStudied += (int) TimeUnit.MILLISECONDS.toSeconds(stop - lastStart);
+            setTotalStudied(totalStudied);
         }
         lastStart = null;
     }
 
+    private void setTotalStudied(int totalStudied) {
+        String key = mDateFormat.format(new Date());
+        mStorage.put(key, totalStudied);
+    }
+
     public int getTotalStudied() {
-        return totalStudied;
+        String key = mDateFormat.format(new Date());
+        return mStorage.containsKey(key) ? mStorage.get(key) : 0;
     }
 }
