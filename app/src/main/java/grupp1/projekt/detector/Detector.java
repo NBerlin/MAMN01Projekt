1d1998c790c583b630fe3d2c04970b4316db0ccb
Oskar Damkjaer <oskar.damkjaer@gmail.com>
2018-05-15 10:57:35 +0200

    now silent

--- app/src/main/java/grupp1/projekt/detector/Detector.java
+++ app/src/main/java/grupp1/projekt/detector/Detector.java
@@ -1,6 +1,10 @@
 package grupp1.projekt.detector;
 
+import android.app.NotificationManager;
 import android.content.Context;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.os.Build;
 import android.util.Log;
 
 import java.util.ArrayList;
@@ -53,9 +57,30 @@ public class Detector implements SensorFenceListener {
 
         SensorEnums outState = isInside ? SensorEnums.INSIDE : SensorEnums.OUTSIDE;
 
+        setSilent(isInside, mContext);
         for (DetectorListener listener : mListeners) {
             listener.onStateChange(outState);
         }
     }
 
+    private void setSilent(boolean silent, Context context) {
+        NotificationManager notificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (!notificationManager.isNotificationPolicyAccessGranted()) {
+            Intent intent = new Intent(android.provider.Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS);
+            mContext.startActivity(intent);
+        }
+
+        if (!notificationManager.isNotificationPolicyAccessGranted()) {
+            AudioManager audio = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+            if (audio != null) {
+                if (silent) {
+                    audio.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+                } else {
+                    audio.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+                }
+            }
+        }
+    }
 }
